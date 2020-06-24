a04858863d0da49f8a77c64573997f0fc5e8fb58
nidhish naghera <nidhishnaghera5@outlook.com>
2020-06-24 15:22:23 +0530

    Updates

---
+++ precache-manifest.b5f9136a3c7e40911b1501db6ae460f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db1ce5189cc17871d5e2c65e238d7e3",
+    "url": "/RouterTestNidhish/index.html"
+  },
+  {
+    "revision": "470877030acfdf7aab90",
+    "url": "/RouterTestNidhish/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "96f165f2584af8c195e3",
+    "url": "/RouterTestNidhish/static/js/2.5a43f3e1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/RouterTestNidhish/static/js/2.5a43f3e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "470877030acfdf7aab90",
+    "url": "/RouterTestNidhish/static/js/main.92f0e7be.chunk.js"
+  },
+  {
+    "revision": "c9f1374b5ee0b059526d",
+    "url": "/RouterTestNidhish/static/js/runtime-main.7480a240.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RouterTestNidhish/static/media/logo.5d5d9eef.svg"
+  }
+]);
